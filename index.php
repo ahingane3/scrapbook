0d648fda8af212257c360b3fa527691dc75dda07
ahingane3 <82169540+ahingane3@users.noreply.github.com>
2021-04-08 21:35:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="marquee" style="background-color: rgb(204, 109, 0);">
+        <marquee style="transition: 0.5ms;">
+            <h1><span style="color: aqua;">Scrap</span><span style="color: rgb(229, 226, 226);">Book</span></h1>
+        </marquee>
+    </div>
+    <div class="nav-bar">
+        <ul>
+            <li><a href="index.php ">Home</a></li>
+            <li><a href="Form.php">Form</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+        <div>
+
+        </div>
+        <div class="d2" style=" height: auto; width: 50%;">
+            <img src="img1.jpg" alt="" height="355px" ; width="630px" ; style="margin-right: 32px ; margin-top: 120px;">
+            <br><br><br><br>
+            <p>
+                <h2 class="h2" style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;font-size: 48px; color: rgb(253, 141, 29);">
+                    Memorable Feelings
+                </h2>
+                <p style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size: 27px; text-shadow: 1px 2px 3px rgb(112, 2, 2);">
+                    <br><i> Think one of the <br> greatest feelings in the world <br>is when someone<br> openly tells you how<br> much you mean to them,<br>Stuff like that is so fare.
+                    <br><br>
+                    </i></p>
+            </p>
+
+        </div>
+        <div class="d1 " style=" height: auto; width: 50%; ">
+            <p class="p2 ">
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <i>Scrapbooking, a crafting and documenting activity,<br> involves taking books with blank pages and adding<br> photos, memorabilia, journaling, and embellishments.<br> Scrapbooking is popular because it lets people<br> document their families' lives and socialize with
+            others<br> while crafting.
+            </i>
+            </p>
+            <br><br><br>
+            <br><br><br><br>
+            <img src="img.jpg " alt=" " height="355px " ;width="500px ">
+
+        </div>
+
+
+
+</body>
+
+</html>
